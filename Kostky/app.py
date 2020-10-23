cbfdb65cbab08e4176226daa2ca0dea1a7c1b48d
TheLavk <64014043+TheLavk@users.noreply.github.com>
2020-10-23 18:18:12 +0200

    Add files via upload

--- Kostky/app.py
+++ Kostky/app.py
@@ -1,1 +1,33 @@
+import random
+from flask import Flask, render_template, session
+from flask_session import Session
 
+app = Flask(__name__)
+app.debug = True
+app.secret_key ='sdfasfc25yxvy'
+SESSION_TYPE ='filesystem'
+app.config.from_object(__name__)
+Session(app)
+
+@app.route('/')
+
+def hod_kostkou():
+    hod = random.randint(1,6)
+    hodpc = random.randint(1,6)
+
+    if not 'score_a' in session:
+        session['score_a'] = 0
+    if not 'score_b' in session:
+        session['score_b'] = 0
+    if hod > hodpc:
+        session['score_a'] = session['score_a'] + 1
+
+    elif hod < hodpc:
+        session['score_b'] = session['score_b'] + 1
+
+
+
+    return render_template('index.html', hod=hod, hodpc=hodpc, score_a=session['score_a'], score_b=session['score_b'] )
+
+if __name__ == '__main__':
+    app.run()
